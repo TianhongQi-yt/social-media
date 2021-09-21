7661fc53d4948a294fff2eb1e5bfb4a60b9d5c1e
Tianhong <ytqth@outlook.com>
2021-09-20 23:08:27 -0700

    add basic post component

---
+++ components/Post/CardPost.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  Card,
+  Icon,
+  Image,
+  Divider,
+  Segment,
+  Button,
+  Popup,
+  Header,
+  Modal,
+} from "semantic-ui-react";
+import PostComments from "./PostComments";
+import CommentInputField from "./CommentInputField";
+import calculateTime from "../../utils/calculateTime";
+import Link from "next/link";
+
+const CardPost = () => {
+  const [likes, setLikes] = useState(post.likes);
+  const isLiked =
+    likes.length > 0 &&
+    likes.filter((like) => like.user === user._id).length > 0;
+  const [comments, setComments] = useState(post.comments);
+  const [error, setError] = useState(null);
+
+  return (
+    <>
+      <Segment basic> </Segment>
+    </>
+  );
+};
+
+export default CardPost;
---
+++ components/Post/CommentInputField.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CommentInputField = () => {
+    return <div></div>
+}
+
+export default CommentInputField;
