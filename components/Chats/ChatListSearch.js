09a0a676894ee3468b9dcdf662750312fde78d3d
TianhongQi-yt <tqi@usc.edu>
2021-10-26 11:09:40 -0700

    fix chats search error

--- components/Chats/ChatListSearch.js
+++ components/Chats/ChatListSearch.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { List, Image, Search } from "semantic-ui-react";
 import axios from "axios";
 import cookie from "js-cookie";
@@ -12,9 +12,12 @@ function ChatListSearch({ chats, setChats }) {
   const [results, setResults] = useState([]);
   const router = useRouter();
 
-  const handleChange = async (e) => {
+  const handleChange = async e => {
     const { value } = e.target;
     setText(value);
+    if (value.length === 0) return;
+    if (value.trim().length === 0) return;
+
     setLoading(true);
 
     try {
@@ -24,44 +27,50 @@ function ChatListSearch({ chats, setChats }) {
 
       const res = await axios.get(`${baseUrl}/api/search/${value}`, {
         headers: { Authorization: token },
-        cancelToken: new CancelToken((canceler) => {
+        cancelToken: new CancelToken(canceler => {
           cancel = canceler;
-        }),
+        })
       });
 
-      if (res.data.length === 0) return setLoading(false);
+      if (res.data.length === 0) {
+        results.length > 0 && setResults([]);
+
+        return setLoading(false);
+      }
 
       setResults(res.data);
     } catch (error) {
-      alert("Error Searching");
+      console.log(error);
     }
 
     setLoading(false);
   };
 
-  const addChat = (result) => {
+  const addChat = result => {
     const alreadyInChat =
       chats.length > 0 &&
-      chats.filter((chat) => chat.messagesWith === result._id).length > 0;
+      chats.filter(chat => chat.messagesWith === result._id).length > 0;
 
-    // 是否有聊天窗口，若没有，创建新的聊天窗
     if (alreadyInChat) {
       return router.push(`/messages?message=${result._id}`);
-    }
-    else {
+    } else {
       const newChat = {
         messagesWith: result._id,
         name: result.name,
         profilePicUrl: result.profilePicUrl,
         lastMessage: "",
-        date: Date.now(),
+        date: Date.now()
       };
 
-      setChats((prev) => [newChat, ...prev]);
+      setChats(prev => [newChat, ...prev]);
 
       return router.push(`/messages?message=${result._id}`);
     }
   };
+
+  useEffect(() => {
+    if (text.length === 0 && loading) setLoading(false);
+  }, [text]);
 
   return (
     <Search
